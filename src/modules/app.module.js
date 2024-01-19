bbfa4c652be2d00c04b84a2ffb8da5f373d0bc6c
FirdavsCoder <firdavsyorkulov@gmail.com>
2024-01-19 12:41:02 +0500

    Created School CRUD

--- src/modules/app.module.js
+++ src/modules/app.module.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import user from "./user/user.module.js";
 import brandModule from "./brand/brand.module.js";
+import schoolModule from "./school/school.module.js";
+
 
 const router = Router();
 
 router.use("/user", user.router);
-router.use("/brand", brandModule.brandRouter)
+router.use("/brand", brandModule.router);
+router.use("/school", schoolModule.router)
 
 export default { router };
